c4cb96ccac6ac7232abc4599822489b262850b2c
buchert70427 <buchert70427@B124-19>
2025-04-15 10:07:49 -0400

    Custom exceptions

--- lab15A/lab15A.cpp
+++ lab15A/lab15A.cpp
@@ -4,3 +4,24 @@
 *Lab 15 A
 *4/15/2025
 ***********/
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+
+//Custom exceptions
+class invalidCharacterException {
+public:
+	const char* what()const noexcept {
+		return "Invalid character: Input must be a letter (A-Z or a-z)";
+	}
+};
+
+class invalidRangeException {
+public:
+	const char* what()const noexcept {
+		return "Invalid range: Resulting character is not a valid letter or crosses case boundaries";
+	}
+};
